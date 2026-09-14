e8521e1610ec02a2af7350bd8e2214a0816741ed
AIR\vantan <suzuho9@gmail.com>
2026-09-14 17:49:34 +0900

    no message

--- Assets/script/PlayerMove.cs
+++ Assets/script/PlayerMove.cs
@@ -15,7 +15,13 @@ public class PlayerMove : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        float mx = Input.GetAxis("Mouse X");
+        float my = Input.GetAxis("Mouse Y");
+        if (Mathf.Abs(mx) > 0.001f)
+        {
+            transform.RotateAround(transform.position, Vector3.up, mx);
 
+        }
         Vector3 movement = new Vector3(x, 0.0f, z);
         rb.linearVelocity = movement * speed;
     }
